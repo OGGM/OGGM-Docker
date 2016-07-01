f50a5c58e52a9831c31d136e88a721bd9caf715a
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-07-01 13:30:43 +0200

    Get rid of progressbar2 entirely until it's fixed

--- base/test.sh
+++ base/test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 /usr/bin/pip3 install git+https://github.com/OGGM/oggm.git &&
-	/usr/bin/pip3 uninstall progressbar2 &&
 	/usr/bin/python3 -c 'import nose, os, sys, oggm; os.environ["OGGM_SLOW_TESTS"] = "True"; os.chdir(os.path.dirname(os.path.abspath(oggm.__file__))); nose.main(argv=["--detailed-errors", "--exe"])'
