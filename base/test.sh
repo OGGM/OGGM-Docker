c94af5180d83cc157ad76321057987a73398f035
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-07-01 13:19:43 +0200

    Avoid progressbar2 for now

--- base/test.sh
+++ base/test.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-/usr/bin/pip3 install git+https://github.com/OGGM/oggm.git && /usr/bin/python3 -c 'import nose, os, sys, oggm; os.chdir(os.path.dirname(os.path.abspath(oggm.__file__))); nose.main(argv=["--detailed-errors", "--exe"])'
+/usr/bin/pip3 install git+https://github.com/OGGM/oggm.git &&
+	/usr/bin/pip3 uninstall progressbar2 &&
+	/usr/bin/python3 -c 'import nose, os, sys, oggm; os.environ["OGGM_SLOW_TESTS"] = "True"; os.chdir(os.path.dirname(os.path.abspath(oggm.__file__))); nose.main(argv=["--detailed-errors", "--exe"])'
