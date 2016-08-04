a3eadbd7af8c8b2d7752c25fc8493aea4c38c69c
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2016-08-04 14:49:56 +0200

    Symlinks don't work on Dockerhub

--- untested_base/test.sh
+++ untested_base/test.sh
@@ -1,1 +1,3 @@
-../base/test.sh
+#!/bin/sh
+/usr/bin/pip3 install git+https://github.com/OGGM/oggm.git &&
+	/usr/bin/python3 -c 'import nose, os, sys, oggm; os.environ["OGGM_SLOW_TESTS"] = "True"; os.chdir(os.path.dirname(os.path.abspath(oggm.__file__))); nose.main(argv=["--detailed-errors", "--exe"])'
