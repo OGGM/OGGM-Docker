c3312e1e66e6284514ab5aea4eff2f779259d75e
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2017-06-23 13:54:53 +0200

    Update for new deps

--- update_base.sh
+++ update_base.sh
@@ -9,10 +9,8 @@ function getSha() {
 cd "$(dirname "$0")"
 
 SHA_SALEM="$(getSha fmaussion/salem)"
-SHA_CLEO="$(getSha fmaussion/cleo)"
 
 sed -i -r "s|salem\.git@[A-Za-z0-9]+|salem.git@${SHA_SALEM}|" base/Dockerfile || exit -2
-sed -i -r "s|cleo\.git@[A-Za-z0-9]+|cleo.git@${SHA_CLEO}|" base/Dockerfile || exit -2
 
 cp base/Dockerfile untested_base/Dockerfile
 cp base/test.sh untested_base/test.sh
