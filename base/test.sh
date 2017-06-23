36e34ba00eb9fac04fa7d3e28e70a6366b32d0f3
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2017-06-23 15:02:52 +0200

    Remove potential spurious output of sample data download

--- base/test.sh
+++ base/test.sh
@@ -2,4 +2,4 @@
 set -e
 
 python3 -c 'import oggm' 2>/dev/null || /usr/bin/pip3 install git+https://github.com/OGGM/oggm.git
-pytest "$(python3 -c 'import oggm, os; print(os.path.dirname(oggm.__file__))')" --mpl
+pytest "$(python3 -c 'import oggm, os; print(os.path.dirname(oggm.__file__))' | tail -n1)" --mpl
