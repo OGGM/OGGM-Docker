574f1a7bd562b74468f5bacd1e7d100dcf57dfed
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-06-13 11:43:21 +0200

    Update to Ubuntu 18.04

--- base/test.sh
+++ base/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-python3.6 -c 'import oggm' 2>/dev/null || python3.6 -m pip install git+https://github.com/OGGM/oggm.git
-pytest "$(python3.6 -c 'import oggm, os; print(os.path.dirname(oggm.__file__))' | tail -n1)" --mpl
+python3 -c 'import oggm' 2>/dev/null || python3 -m pip install git+https://github.com/OGGM/oggm.git
+pytest "$(python3 -c 'import oggm, os; print(os.path.dirname(oggm.__file__))' | tail -n1)" --mpl
