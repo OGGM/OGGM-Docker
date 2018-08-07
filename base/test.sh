766c156d8b4d3065433842db543fc5e2dfa16d97
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-08-07 15:35:06 +0200

    Update test.sh

--- base/test.sh
+++ base/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-python3 -c 'import oggm' 2>/dev/null || python3 -m pip install git+https://github.com/OGGM/oggm.git
-pytest "$(python3 -c 'import oggm, os; print(os.path.dirname(oggm.__file__))' | tail -n1)" --mpl
+git clone https://github.com/OGGM/oggm.git /tmp/oggm_git
+cd /tmp/oggm_git
+pip install -I -e .
+pytest --mpl oggm
