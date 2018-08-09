204ed60bd66d09d041c69aa02a6f1cedef4328f6
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-08-09 12:57:09 +0200

    Don't pointlessly build base twice

--- base/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-git clone https://github.com/OGGM/oggm.git /tmp/oggm_git
-cd /tmp/oggm_git
-"$PIP" install -I -e .
-pytest --mpl oggm
--- untested_base/test.sh
+++ untested_base/test.sh
@@ -4,4 +4,7 @@ set -e
 git clone https://github.com/OGGM/oggm.git /tmp/oggm_git
 cd /tmp/oggm_git
 "$PIP" install -I -e .
-pytest --mpl oggm
+mkdir fake_home
+HOME=/tmp/oggm_git/fake_home pytest --mpl oggm
+cd -
+rm -rf /tmp/oggm_git
