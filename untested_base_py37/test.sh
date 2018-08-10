afac78fc34e5f30f36290428b1b171fa11d83a47
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2018-08-10 23:48:37 +0200

    Fix test-pip dependency installation

--- untested_base_py37/test.sh
+++ untested_base_py37/test.sh
@@ -3,7 +3,7 @@ set -e
 
 git clone https://github.com/OGGM/oggm.git /tmp/oggm_git
 cd /tmp/oggm_git
-"$PIP" install -I -e .
+"$PIP" install --no-deps --ignore-installed -e .
 mkdir fake_home
 HOME=/tmp/oggm_git/fake_home pytest --mpl oggm
 cd -
