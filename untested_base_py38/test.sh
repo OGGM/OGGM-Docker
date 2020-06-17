f833d16adb16ccfcdaa25b1b9f6b445cf8a234d6
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2020-06-17 17:35:47 +0200

    Fix test.sh

--- untested_base_py38/test.sh
+++ untested_base_py38/test.sh
@@ -3,8 +3,8 @@ set -e
 
 git clone https://github.com/OGGM/oggm.git /tmp/oggm_git
 cd /tmp/oggm_git
-"$PIP" install --no-deps --ignore-installed -e .
+$PIP install --no-deps --ignore-installed -e .
 mkdir fake_home
-HOME=/tmp/oggm_git/fake_home pytest --mpl-oggm --mpl-upload oggm
+HOME=/tmp/oggm_git/fake_home pytest --mpl-oggm --mpl-upload --pyargs oggm
 cd -
 rm -rf /tmp/oggm_git
