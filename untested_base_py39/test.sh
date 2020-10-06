c19e94548c22cb4f328ae77ecc18e9fb55f6c4a8
BtbN <btbn@btbn.de>
2020-10-06 16:56:20 +0200

    Add Python 3.9 image

---
+++ untested_base_py39/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/OGGM/oggm.git /tmp/oggm_git
+cd /tmp/oggm_git
+$PIP install --no-deps --ignore-installed -e .
+mkdir fake_home
+HOME=/tmp/oggm_git/fake_home pytest --mpl-oggm --mpl-upload --pyargs oggm
+cd -
+rm -rf /tmp/oggm_git
